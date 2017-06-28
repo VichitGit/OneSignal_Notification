9d5adb1bc0ed4f2d77c68a96c0f002b90d2b7e40
VichitGit <v.vichitpov@gmail.com>
2017-06-28 14:44:42 +0700

    Create Project

---
+++ app/src/main/java/developer/vichit/android/com/onesignal/MainActivity.java
@@ -0,0 +1,13 @@
+package developer.vichit.android.com.onesignal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
